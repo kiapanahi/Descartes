6ea98ac27b19aab04835d8d0a70cc531470b00bb
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-11 10:51:40 +0430

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ class Program {
     public static Main(): void {
         let parser = new TextParser(true);
 
-        parser.ParseText("Dear {user.name}! Welcome to the Matrix. Our contact is: {company.email}");
+        parser.ParseText("Dear {user.name}! Welcome to the {location.name}. Our contact is: {company.email}");
         // parser.ParseText("Dear {user.name! Welcome to the Matrix. Our contact is: {company.email}");
         // parser.ParseText("Dear user.name}! Welcome to the Matrix. Our contact is: {company.email}");
         // parser.ParseText("{user.email}Dear! Welcome to the Matrix. Our contact is: {company.email}");
